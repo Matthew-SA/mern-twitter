31661412a6a4a0a87c546fb6c14de1dec5c7c683
Matthew-SA <surfsnow1@gmail.com>
2020-01-21 00:31:00 -0800

    backend working - frontend still having issues?

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
   
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,3 +21,13 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
