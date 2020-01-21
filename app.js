532aed2273f3a50a79e83e060500ebc96d579b99
Matthew-SA <surfsnow1@gmail.com>
2020-01-20 21:24:09 -0800

    add debugger capability

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const app = express();
 const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 
 
-const app = express();
-const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
